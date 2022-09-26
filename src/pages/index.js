c201c970d5eebe534eaba49c7a2d29baeb468ccc
93chanle <38956395+93chanle@users.noreply.github.com>
2022-09-26 16:29:26 +0200

    2 files modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,16 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
+var abstractText =
+  'The Mobile Solution Workshop is a new approach for participative workshops developed at the Faculty of Sustainability. This digital environment allows to visualise future scenarios and to experience the effects of adjusted model parameters in real time. It consists of the hardware, including five 65’’ screens that create an immersive environment and tablets that allow participants to interact with the screens’ content, as well as the custom developed software for this interactive dashboard. The entire set-up is mobile and can therefore be put into action on site. For this reason alone, scientist can reach a larger target audience with the Mobile Solution Workshop. Complex problems are fanned out and visualised in comprehensible steps across the five connected 65’’ screens. Participants can experience and design interactive scenarios. To this end, the Mobile Solution Workshop allows to visualise effects of interventions in real-time (overcoming temporal boundaries); illustrates the interplay between local actions and global drivers (overcoming spatial boundaries) and to make abstract facts tangible (overcoming the gap between abstract knowledge and concrete planning, decision making and action).';
+
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className={'container'}>
-        <h1 className="hero__title">{siteConfig.title}</h1>
+    <header className={'header'}>
+      <div className={clsx('container', styles.heroBanner)}>
+        <h1 className="hero__title">{'Mobile Solution Workshop'}</h1>
+        <h1 className="hero__title__2">{'Mobile Lösungswerkstatt (MoLöwe)'}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.buttons}>
@@ -23,6 +27,9 @@ function HomepageHeader() {
             To Documentation & Tutorial
           </Link>
         </div>
+      </div>
+      <div className={clsx('container', styles.abstractBanner)}>
+        <p className="abstract">{abstractText}</p>
       </div>
     </header>
   );
